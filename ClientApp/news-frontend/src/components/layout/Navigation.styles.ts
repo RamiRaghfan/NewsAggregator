3adab5dce3c26388a175522c1c8299f74a539fb3
RamiRaghfan <rami.raghfan@gmail.com>
2023-12-15 02:19:37 +0200

    Testing Merge

---
+++ ClientApp/news-frontend/src/components/layout/Navigation.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  background-color: #333;
+  padding: 10px;
+`;
+
+export const CategorySelect = styled.select`
+  padding: 5px;
+  margin-right: 10px;
+`;
+
+export const SearchInput = styled.input`
+  padding: 5px;
+`;
