6d593685eab6359ab16fe833a5c3df7be98e78c7
RamiRaghfan <rami.raghfan@gmail.com>
2024-02-07 01:42:33 +0200

    added placeholders

--- ClientApp/news-frontend/src/components/layout/NavigationBar/Navigation.styles.ts
+++ ClientApp/news-frontend/src/components/layout/NavigationBar/Navigation.styles.ts
@@ -2,6 +2,32 @@
 import styled from 'styled-components';
 import { ThemeType } from '../../themes/themes';
 
+
+export const LogoPlaceholder = styled.div`
+  height: 50px; // Adjust the height as needed
+  width: 50px; // Adjust the width as needed
+  background-color: #ccc; // This is just a placeholder, use an actual image or logo here
+  margin-right: 20px; // Spacing between the logo and the category buttons
+`;
+
+
+export const SearchButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+export const UserButtonPlaceholder = styled.div`
+  height: 30px; // Adjust the size as needed
+  width: 30px; // Adjust the size as needed
+  background-color: #ccc; // Placeholder color, replace with actual content or icon
+  border-radius: 50%; // Makes it a circle
+  margin-left: 15px; // Space from the previous element
+`;
+
+export const NotificationsButtonPlaceholder = styled(UserButtonPlaceholder)`
+`;
+
 export const Nav = styled.nav<{ theme: ThemeType }>`
 
   background-color: ${props => props.theme.background};
